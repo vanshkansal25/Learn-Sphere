a441cbfe0210bac18a0b28b1bd34598a93380a18
@vanshkansal25 <kansalv578@gmail.com>
2025-09-24 16:10:31 +0530

    feat: implement addQuestion and addAnswer functionality with error handling and email notifications

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -6,6 +6,10 @@ import { createCourse } from "../services/course.services";
 import courseModel from "../models/course.model";
 import { triggerAsyncId } from "async_hooks";
 import { redis } from "../utils/redis";
+import mongoose from "mongoose";
+import ejs from "ejs";
+import path from "path";
+import sendEmail from "../utils/sendMail";
 
 
 export const uploadCourse = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
@@ -134,3 +138,105 @@ export const getCourseByUser = asyncHandler(async(req:Request,res:Response,next:
       throw new ErrorHandler(error.message, 400);
     }
 })
+
+interface IAddQuestionData{
+    question:string;
+    courseId:string;
+    contentId:string;
+}
+
+export const addQuestion = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const {question,courseId,contentId}:IAddQuestionData = req.body;
+        const course = await courseModel.findById(courseId);
+        if(!mongoose.Types.ObjectId.isValid(contentId)){
+             throw new ErrorHandler("Invalid Content Id", 400);
+        }
+        const courseContent = course?.courseData?.find((item:any)=>item._id.equals(contentId));
+        if(!courseContent){
+            throw new ErrorHandler("Invalid Course Content", 400);
+        }
+        const newQuestion :any = {
+            user:req.user,
+            question,
+            questionReplies:[],
+        }
+        courseContent.questions.push(newQuestion)
+
+        await course?.save();
+        res.status(200).json({
+            success:true,
+            course
+        })
+    } catch (error: any) {
+      throw new ErrorHandler(error.message, 400);
+    }
+})
+
+interface IAddAnswerData{
+    answer:string,
+    courseId:string,
+    contentId:string,
+    questionId:string
+}
+
+export const addAnswer = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const {answer,courseId,contentId,questionId}:IAddAnswerData = req.body;
+        const course = await courseModel.findById(courseId);
+        if(!mongoose.Types.ObjectId.isValid(contentId)){
+             throw new ErrorHandler("Invalid Content Id", 400);
+        }
+        const courseContent = course?.courseData?.find((item:any)=>item._id.equals(contentId));
+        if(!courseContent){
+            throw new ErrorHandler("Invalid Course Content", 400);
+        }
+        const question = courseContent?.questions?.find((item:any)=>{
+            item._id.equals(questionId);
+        })
+        if(!question){
+            throw new ErrorHandler("Invalid Question Id", 400);
+        }
+        const newAnswer:any = {
+            user:req.user,
+            answer,
+        }
+        question.questionReplies?.push(newAnswer);
+        await course?.save();
+
+        if(req.user?._id === question.user._id){
+            // create a notification
+        }else{
+            const data = {
+                name:question.user.name,
+                title:courseContent.title,
+            }
+            const html = await ejs.renderFile(path.join(__dirname,"../mails/question-reply.ejs"),data)
+            try {
+                await sendEmail({
+                    email:question.user.email,
+                    subject:"Question Reply",
+                    template: "question-reply.ejs",
+                    data,
+                })
+            } catch (error:any) {
+                throw new ErrorHandler(error.message, 500);
+            }
+        }
+        res.status(200).json({
+            success:true,
+            course
+        })
+        
+
+    } catch (error: any) {
+      throw new ErrorHandler(error.message, 400);
+    }
+})
+
+interface IReviewData{
+    review:string,
+    courseId:string,
+    rating:number,
+    userId:string
+}
