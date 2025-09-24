34b165e9112717f2a3d57d44533e25527eb8c21e
@vanshkansal25 <kansalv578@gmail.com>
2025-09-24 15:43:41 +0530

    feat: add question and answer functionality to course routes and update comment schema

--- models/course.model.ts
+++ models/course.model.ts
@@ -1,9 +1,10 @@
 import mongoose,{Document,Model,model,Schema} from "mongoose";
+import { IUser } from "./user.model";
 
 interface IComment extends Document{
-    user:object;
-    comment:string;
-    commentReplies?:IComment[];
+    user:IUser;
+    question:string;
+    questionReplies?:IComment[];
 }
 interface IReview extends Document{
     user:object;
@@ -57,8 +58,8 @@ const linkSchema = new Schema<ILink>({
 })
 const commentSchema = new Schema<IComment>({
     user:Object,
-    comment:String,
-    commentReplies:[Object],
+    question:String,
+    questionReplies:[Object],
 })
 const courseDataSchema = new Schema<ICourseData>({
     title:String,
